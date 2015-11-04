5800f9c35e0c6077d4ed2d90a21968af66b8779a
kathHen <kath.henderson@tombola.com>
2015-11-04 16:58:46 +0000

    getting rid of the comments

--- main-app/app/scripts/login/login-service.js
+++ main-app/app/scripts/login/login-service.js
@@ -2,51 +2,6 @@
     'use strict';
     angular.module('Tombola.BingoClient.Login')
         .service('LoginService', ['$http', function($http){
-            //var me = this;
 
-                //function dataCall (data){
-                //    var req = {
-                //        method: 'POST',
-                //        url: 'http://eutaveg-01.tombola.emea:30069/users/login',
-                //        headers: {
-                //            'Accept': 'application/json',
-                //            'Content-Type': 'application/json'
-                //        },
-                //        data: data
-                //    };
-                //    return $http(req);
-                //}
-
-           // me.login = function(username, password){
-           //     dataCall({username: username, password: password})
-           //         .then(function(response){
-           //             console.log(response);
-           //         })
-           //         .catch(function(response){
-           //             console.log(response);
-           //         });
-           //};
-
-            //var login = function (username, password){
-            //    var deferred = $q.defer();
-            //
-            //    $http.post('http://eutaveg-01.tombola.emea:30069/users/login', {
-            //        username: username,
-            //        password: password
-            //    }).then(function(result){
-            //        userInfo = {
-            //            accessToken: result.data.acesss_token,
-            //            username: result.data.username
-            //        };
-            //        $window.sessionStorage.userInfo = JSON.stringify(userInfo);
-            //        deferred.resolve(userInfo);
-            //    }, function(error){
-            //        deferred.reject(error);
-            //    });
-            //    return deferred.promise();
-            //};
-            //return{
-            //    login: login
-            //};
         }]);
 })();
